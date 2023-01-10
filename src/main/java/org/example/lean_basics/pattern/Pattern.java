b2b70550c1bd736936ddd39bbc9d0172673d3c18
PremVarma01 <105207883+PremVarma01@users.noreply.github.com>
2023-01-10 22:06:12 +0530

    Added initial java setup for project

---
+++ src/main/java/org/example/lean_basics/pattern/Pattern.java
@@ -0,0 +1,48 @@
+package org.example.lean_basics.pattern;
+
+public class Pattern {
+    public static void main(String[] args) {
+        System.out.println("Pattern");
+        Pattern1();
+    }
+
+    public static void Pattern1(){
+
+    }
+
+    public static void Pattern2(){
+
+    }
+
+    public static void Pattern3(){
+
+    }
+
+    public static void Pattern4(){
+
+    }
+
+    public static void Pattern5(){
+
+    }
+
+    public static void Pattern6(){
+
+    }
+
+    public static void Pattern7(){
+
+    }
+
+    public static void Pattern8(){
+
+    }
+
+    public static void Pattern9(){
+
+    }
+
+    public static void Pattern10(){
+
+    }
+}
