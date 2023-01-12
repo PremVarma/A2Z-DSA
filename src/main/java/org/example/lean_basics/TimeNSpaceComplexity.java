f0c2ebc42b0c8a57ea42230c912781db58753684
PremVarma01 <105207883+PremVarma01@users.noreply.github.com>
2023-01-12 12:53:01 +0530

    Basic Math and Pattern problems

---
+++ src/main/java/org/example/lean_basics/TimeNSpaceComplexity.java
@@ -0,0 +1,17 @@
+package org.example.lean_basics;
+
+
+/*-------------------Time Complexity -------------------------*/
+// Rate at which time taken increases with input size.
+//Rules -> Always compute worst case, Avoid constant, Avoid lower values in TC
+//Worst Case - BigO , Average Case - ThetaO, Best Case - OmegaO
+
+/*-------------------Space Complexity-------------------------*/
+// Auxilary Space ( Space take to solve problem ) + Input Space (Space to store input)
+
+// Important -> If No of iteration is depends on division time complexity will be logarithmic
+public class TimeNSpaceComplexity {
+    public static void main(String[] args) {
+
+    }
+}
